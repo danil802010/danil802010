bafe496061ba488bf4e988521c8a5e9dfbe18a98
danil802010 <danil802010@gmail.com>
2024-04-19 16:20:25 +0300

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 let grob;
 grob='<table><td id="lev"><h4>poleveem</h4>'
-  let a,b,c;
-a="привет, ",b="животное, ",c="как дела?";
-alert(a+b+c);
+ 
 +'</td><td id="lpcool"></td>'
 +'<td id="prav"><h4>popravimsja</h4></td></table>'
 
